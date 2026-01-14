5e947e62e7f2241a709c4976c34726e325d43bf5
Kimberly Michela <141520544+kmbrlym@users.noreply.github.com>
2026-01-13 20:01:19 -0500

    Update solution for invert-binary-tree

--- leetcode/invert-binary-tree.py
+++ leetcode/invert-binary-tree.py
@@ -1,1 +1,25 @@
-9123›[4,2,7,1,3,6,9][2,1,3][]
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: Optional[TreeNode]
+        """
+        if not root:
+            return None
+        q = [root]
+        while q:
+            next_level = []
+            for i in q:
+                if i.left:
+                    next_level.append(i.left)
+                if i.right:
+                    next_level.append(i.right)
+                i.left, i.right = i.right, i.left
+            q = next_level
+        return root
