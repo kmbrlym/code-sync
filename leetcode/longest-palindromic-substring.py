8399faed9dcb792558823f20bff2ff1c9fbe1230
Kimberly Michela <141520544+kmbrlym@users.noreply.github.com>
2026-01-15 12:19:48 -0500

    Add solution for longest-palindromic-substring

---
+++ leetcode/longest-palindromic-substring.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        resL = 0
+        resR = 0
+        resLen = 0
+        for i in range(len(s)):
+            l, r = i, i
+            while l>= 0 and r<len(s) and s[l] == s[r]:
+                    if (r-l+1) > resLen:
+                        resLen = r-l+1
+                        resL = l
+                        resR = r
+                    l -= 1
+                    r += 1
+            l, r = i, i+1
+            while l>= 0 and r<len(s) and s[l] == s[r]:
+                    if (r-l+1) > resLen:
+                        resLen = r-l+1
+                        resL = l
+                        resR = r
+                    l -= 1
+                    r += 1
+        return s[resL:resR+1]
