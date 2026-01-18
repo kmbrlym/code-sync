44d58974eb92324623b6834709bf6664b374344a
Kimberly Michela <141520544+kmbrlym@users.noreply.github.com>
2026-01-18 16:20:35 -0500

    Update solution for valid-anagram

--- leetcode/valid-anagram.py
+++ leetcode/valid-anagram.py
@@ -3,15 +3,16 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         hash_s = Counter(s)
         hash_t = Counter(t)
-        set_s = set()
-        for i in hash_s:
-            set_s.add(i)
-            if i in hash_t:
-                if hash_t[i] != hash_s[i]:
-                    return False
-            else:
-                return False
-        set_t = set()
-        for i in hash_t:
-            set_t.add(i)
-        return len(set_t) == len(set_s)
+        return hash_s == hash_t
+        # set_s = set()
+        # for i in hash_s:
+        #     set_s.add(i)
+        #     if i in hash_t:
+        #         if hash_t[i] != hash_s[i]:
+        #             return False
+        #     else:
+        #         return False
+        # set_t = set()
+        # for i in hash_t:
+        #     set_t.add(i)
+        # return len(set_t) == len(set_s)
