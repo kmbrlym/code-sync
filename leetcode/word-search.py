d206042412cc72eb171dbb7ad16b1608123d02a0
Kimberly Michela <141520544+kmbrlym@users.noreply.github.com>
2026-01-19 11:36:19 -0500

    Add solution for word-search

---
+++ leetcode/word-search.py
@@ -0,0 +1,18 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        seen = set()
+        row, column = len(board), len(board[0])
+        def dfs(r, c, i):
+            if len(word) == i:
+                return True
+            if r < 0 or c < 0 or r >= row or c >= column or (r,c) in seen or word[i] != board[r][c]:
+                return False
+            seen.add((r,c))
+            res = dfs(r-1, c,i+1) or dfs(r+1, c,i+1) or dfs(r, c-1,i+1) or dfs(r, c+1,i+1)
+            seen.remove((r,c))
+            return res
+        for i in range(row):
+            for j in range(column):
+                if dfs(i,j,0):
+                    return True
+        return False
