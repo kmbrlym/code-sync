28e72b9979290811f68e5296d646436a4b8e6a73
Kimberly Michela <141520544+kmbrlym@users.noreply.github.com>
2026-01-23 19:31:58 -0500

    Update solution for reverse-linked-list

--- leetcode/reverse-linked-list.py
+++ leetcode/reverse-linked-list.py
@@ -1,23 +1,15 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def reverseList(self, head):
-        """
-        :type head: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         curr = head
-        temp = []
+        prev = ListNode()
         while curr is not None:
-            temp.append(curr.val)
+            temp = prev.next
+            prev.next = curr
             curr = curr.next
-        curr = head
-        i = len(temp) - 1
-        while curr is not None:
-            curr.val = temp[i]
-            curr = curr.next
-            i -= 1
-        return head
+            prev.next.next = temp
+        return prev.next
